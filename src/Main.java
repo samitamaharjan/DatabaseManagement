32fb79d64be93002b9f0bc0c98b82c4a3294b9f9
Samita Maharjan <samitamaharjan@gmail.com>
2017-07-04 01:11:08 -0500

    example of finding entry by primarykey

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,28 @@ import java.util.List;
 
 import com.project.manager.FileManager;
 import com.project.manager.FileManagerImpl;
-import com.project.model.Address;
+import com.project.model.Book;
 
 public class Main {
 
 	public static void main(String[] args) {
-		FileManager<Address> addrManager = new FileManagerImpl<>("address");
-		addrManager.clean();
-		Address add1 = new Address("1000N 4th St.", "Fairfield", "52557", "IA");
-		addrManager.insert(add1);
+		FileManager<Book> bookManager = new FileManagerImpl<>("book");
+		bookManager.clean();
+		Book b1 = new Book("1111", "Java", true, null);
+		Book b2 = new Book("2222", "Hadoop", true, null);
+		Book b3 = new Book("3333", "Oracle", true, null);
+		bookManager.insert(b1);
+		bookManager.insert(b2);
+		bookManager.insert(b3);
 		
-		List<Address> addresses = addrManager.findAll();
+		List<Book> books = bookManager.findAll();
 		
-		for (Address addr : addresses) {
-			System.out.printf("%-15s %-15s %-10s %-5s%n",
-				addr.getStreet(), addr.getCity(), addr.getZip(), addr.getState());
+		for (Book b : books) {
+			System.out.printf("%-15s %-15s%n",
+				b.getISBN(), b.getTitle());
 		}
+		
+		Book book = bookManager.findByPrimaryKey("2222");
+		System.out.println("Book found: " + book.getTitle());
 	}
 }
